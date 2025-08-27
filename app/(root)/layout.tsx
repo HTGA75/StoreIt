37f15564a9dd109a233d58f546f55a381e2b9a32
HTGA75 <hg23750@gmail.com>
2025-08-27 12:51:12 +0530

    Worked on the Sidebar UI, navigation UI, gettingCurrentUser details

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+// import MobileNavigation from "@/components/MobileNavigation";
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+import { getCurrentUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
+import MobileNavigation from "@/components/MobileNavigation";
+// import { Toaster } from "@/components/ui/toaster";
+
+export const dynamic = "force-dynamic";
+
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) return redirect("/sign-in");
+
+  return (
+    <main className="flex h-screen">
+      <Sidebar {...currentUser} />
+
+      <section className="flex h-full flex-1 flex-col">
+        <MobileNavigation />
+        <Header />
+        <div className="main-content">{children}</div>
+      </section>
+
+      {/* <Toaster /> */}
+    </main>
+  );
+};
+export default Layout;
