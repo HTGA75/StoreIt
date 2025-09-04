3d45f89478cc5099a75b0bc59476f2778f39d85b
HTGA75 <hg23750@gmail.com>
2025-09-04 16:50:56 +0530

    dashboard-summary of the different types of files occupying the space in the storage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,54 @@
 import { Chart } from "@/components/Chart";
+import FormattedDateTime from "@/components/FormattedDateTime";
 import { getTotalSpaceUsed } from "@/lib/actions/file.actions";
+import { convertFileSize, getUsageSummary } from "@/lib/utils";
+import { Separator } from "@radix-ui/react-separator";
 import Image from "next/image";
+import Link from "next/link";
 
 export default async function Home() {
   const chartData = await getTotalSpaceUsed()
-  console.log(chartData)
+
+  const usageSummary = getUsageSummary(chartData)
+
   return (
-    <>
-      <Chart used={chartData.used} />
-    </>
+    <div className="dashboard-container">
+      <section>
+        <Chart used={chartData.used} />
+
+        <ul className="dashboard-summary-list">
+          {usageSummary.map((summary) => (
+            <Link
+              href={summary.url}
+              key={summary.title}
+              className="dashboard-summary-card"
+            >
+              <div className="space-y-4">
+                <div className="flex justify-between gap-3">
+                  <Image
+                    src={summary.icon}
+                    width={100}
+                    height={100}
+                    alt="uploaded image"
+                    className="summary-type-icon"
+                  />
+                  <h4 className="summary-type-size">
+                    {convertFileSize(summary.size) || 0}
+                  </h4>
+                </div>
+
+                <h5 className="summary-type-title">{summary.title}</h5>
+                <Separator className="bg-light-400" />
+                <FormattedDateTime
+                  date={summary.latestDate}
+                  className="text-center"
+                />
+              </div>
+            </Link>
+          ))}
+        </ul>
+      </section>
+      <section className="dashboard-recent-files"></section>
+    </div>
   );
 }
