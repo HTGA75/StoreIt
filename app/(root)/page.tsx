016d602bdef575ab68d2d4fa3309c946a46b2182
HTGA75 <hg23750@gmail.com>
2025-09-04 16:19:28 +0530

    used chart comp of shadcn to display available storage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,13 @@
+import { Chart } from "@/components/Chart";
+import { getTotalSpaceUsed } from "@/lib/actions/file.actions";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const chartData = await getTotalSpaceUsed()
+  console.log(chartData)
   return (
-    <div className="flex-center h-screen">
-      <h1 className="h1">Welcome to Store It</h1>
-    </div>
+    <>
+      <Chart used={chartData.used} />
+    </>
   );
 }
